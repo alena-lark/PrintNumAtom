af239a0fb3c274b23c182f6a7c10acd84b47c78c
alena-lark <AllyLarck@yandex.com>
2020-10-11 23:14:12 +0300

    fsdahb

--- printnum/PrintNumAtoms.py
+++ printnum/PrintNumAtoms.py
@@ -7,13 +7,4 @@ def __init__(smiles):
     
     a = np.array([1, 2, 3])
     print("inisial step", a)
-
-#    from rdkit import Chem
-#    try:
-#        mol = Chem.AddHs(Chem.MolFromSmiles(smiles))
-#        mol = Chem.MolFromSmiles(smiles)
-#        NumberAtoms = mol.GetNumAtoms()
-#        
-#        return print(NumberAtoms)
-#    except:
-#        return None
+    
