a78bdcfa1c49a0154a6c09cceafb2f1219e20eed
alena-lark <AllyLarck@yandex.com>
2020-10-11 23:08:18 +0300

    sfgab

--- printnum/PrintNumAtoms.py
+++ printnum/PrintNumAtoms.py
@@ -1,9 +1,14 @@
 '''
 Printing number of atoms in molecule
 '''
-from rdkit import Chem
+import numpy as np
 
 def __init__(smiles):
+    
+    a = np.array([1, 2, 3])
+    print("inisial step", a)
+
+    from rdkit import Chem
     try:
 #        mol = Chem.AddHs(Chem.MolFromSmiles(smiles))
         mol = Chem.MolFromSmiles(smiles)
