604e8895277cd75ca035ddf77ce913ef5ee93d3f
alena-lark <AllyLarck@yandex.com>
2020-10-12 22:35:05 +0300

    10.1

--- printnum/__init__.py
+++ printnum/__init__.py
@@ -1,1 +1,3 @@
-from PrintNumAtoms import printnum
+# import a function
+# if file-name, then not recognize
+import printnum
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="printnum",
-    version="0.1.0",
+    version="0.1.1",
     author="alena-lark",
     author_email="AllyLarck@yandex.com",
     description="Printing a number of atoms in a molecule",
